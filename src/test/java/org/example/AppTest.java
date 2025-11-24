de333ece393d270d4be3c4e05ee4f7c7daef3485
Petro <petro_maliuk@epam.com>
2025-11-24 17:39:21 +0200

    Add parallel run config

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,37 +1,32 @@
 package org.example;
 
-import io.restassured.internal.ValidatableResponseImpl;
 import io.restassured.response.ValidatableResponse;
-import io.restassured.response.ValidatableResponseOptions;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-  String endPoint = "https://jsonplaceholder.typicode.com/users";
+  private static final String endPoint = "https://jsonplaceholder.typicode.com/users";
 
   @Test
-  public void testStatusCode(){
-    get().assertThat().statusCode(200);
+  public void testStatusCode() {
+    getRequest().assertThat().statusCode(200);
   }
 
   @Test
-  public void testHeader(){
-    get().assertThat().header("content-type", equalTo("application/json; charset=utf-8"));
+  public void testHeader() {
+    getRequest().assertThat().header("content-type", equalTo("application/json; charset=utf-8"));
   }
 
   @Test
-  public void testBody(){
-    get().assertThat().body("size()", equalTo(10));;
+  public void testBody() {
+    getRequest().assertThat().body("size()", equalTo(10));
   }
 
 
-  public ValidatableResponse get(){
+  private ValidatableResponse getRequest() {
     return given().when().get(endPoint).then();
   }
 }
